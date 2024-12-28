f4792c3fa433cd52d79b4d8c1ce1ffff3acebe81
jeevaofficia7 <jeevaoffic7@gmail.com>
2024-12-28 19:14:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-
 //social icons function
 
-var socialIcons = document.querySelectorAll('#social-icons li ');
+var socialIcons = document.querySelectorAll("#social-icons li ");
 
 //forEach
-socialIcons.forEach((icons=>{
-    icons.addEventListener('click',()=>{
-        icons.classList.toggle('icons-funtion')
-    })
-}))
-
+socialIcons.forEach((icons) => {
+  icons.addEventListener("click", () => {
+    icons.classList.toggle("icons-funtion");
+  });
+});
 
 //Main Btns
-var Buttons = document.querySelectorAll('#btns-container button');
+var Buttons = document.querySelectorAll("#btns-container button");
 
 //forEach
-Buttons.forEach((btn , index)=>{
-    btn.addEventListener('click',()=>{
+Buttons.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
     alert(`you clicked  ${btn.textContent} button.`);
-    })
+  });
 });
 
 //hamburger..
 
-var hamburgerIcon = document.getElementById('hamburger');
-var navLinks = document.getElementById('navlinks');
+var hamburgerIcon = document.getElementById("hamburger");
+var navLinks = document.getElementById("navlinks");
 
-hamburgerIcon.addEventListener('click',()=>{
-    navLinks.classList.toggle('ul-active');
-   if(navLinks.classList.contains('ul-active')){
+hamburgerIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("ul-active");
+  if (navLinks.classList.contains("ul-active")) {
+    hamburgerIcon.innerHTML = "<i class='fa-solid fa-xmark'></i>";
+    //change positin and size..
+    hamburgerIcon.classList.add('x-mark')
+    
+  } else {
+    hamburgerIcon.innerHTML = "<i class='fas fa-bars'></i>";
+    hamburgerIcon.classList.remove('x-mark')
+  }
+});
 
-    hamburgerIcon.innerHTML="<i class='fa-solid fa-circle-xmark'></i>"
-   }
 
-   else{
-    hamburgerIcon.innerHTML="<i class='fas fa-bars'></i>"
-   }
-})
+
